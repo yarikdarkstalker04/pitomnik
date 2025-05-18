6e6c6897409f83c8ed454041f7266ce464d6e3e8
yarikdarkstalker04 <www.yarik90.com@gmail.com>
2025-05-18 08:25:21 +0300

    'pages'

---
+++ pages/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(requset):
+    return HttpResponse("Главная")
+
+def about(request):
+    return HttpResponse('О нас')
+
+def catsMale(requset):
+    return HttpResponse('Коты')
+
+def catsFemale(request):
+    return HttpResponse('Кошки')
+
+def litter(request):
+    return HttpResponse('Помет')
+
+def gelded(request):
+    return HttpResponse('Кострат')
+
+def aboutBreed(request):
+    return HttpResponse('О породе')
+
+def contacts(request):
+    return HttpResponse('Контакты')
